d367796eddaf36a52613f6cf8b5b60b91c8ca2e9
OlaMushroom <ola.mushroom@gmail.com>
2023-05-14 18:00:25 +0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-with streamlit.echo(code_location='below'):
+with streamlit.echo(code_location='above'):
     total_points = streamlit.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = streamlit.slider("Number of turns in spiral", 1, 100, 9)
 
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-
+import streamlit
+with streamlit.echo(code_location='below'):
+    streamlit.video('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
