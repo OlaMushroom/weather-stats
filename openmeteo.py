9de7c986f533b02d3f3a5bf396b8266f13932920
OlaMushroom <ola.mushroom@gmail.com>
2023-06-13 11:48:47 +0700

    optimize

--- openmeteo.py
+++ openmeteo.py
@@ -1,12 +1,6 @@
 import streamlit as st
 #...and some cool modules idk:
 
-#add blank lines (verticle spaces) to the code instead of spamming st.write():
-#from streamlit_extras.add_vertical_space import add_vertical_space as stex_vert_sp
-
-#cooler header:
-#from streamlit_extras.colored_header import colored_header as stex_header
-
 #button that saves its own state:
 #from streamlit_extras.stateful_button import button as stex_button
 
@@ -16,10 +10,11 @@ from streamlit_toggle import st_toggle_switch as stex_switch
 #date picker but with a range selection:
 from streamlit_extras.mandatory_date_range import date_range_picker as stex_dt_range
 
-from datetime import date, datetime, timedelta
-from dateutil.relativedelta import relativedelta
-from dateutil.parser import parse
+from datetime import date
+from dateutil.relativedelta import relativedelta as rltvDelta
 from modules import *
+
+st.title('WEATHER')
 
 #default parameter:
 #'&timeformat=unixtime&timezone=auto'
@@ -38,19 +33,9 @@ def get_date(start, end, min, max, key):
     )
 
 with st.sidebar:
-    st.title('WEATHER')
-    
-    '''
-    stex_header(
-        label = '',
-        description = '',
-        color_name = 'blue-green-70'
-    )
-    '''
-    
     in_opt = st.radio(
         label = 'Search type',
-        options = ('loc', 'coord'),
+        options = ('coord', 'loc', 'ip'),
         format_func = lambda x: dict_loc_opt.get(x),
         horizontal = True,
         label_visibility = 'collapsed',
@@ -58,203 +43,64 @@ with st.sidebar:
         help = ""
     )
 
-    if in_opt == 'loc':
-        loc_opt = st.radio(
-            label = 'Location type',
-            options = ('name', 'ip'),
-            format_func = lambda x: dict_loc_opt.get(x),
-            horizontal = True,
-            label_visibility = 'collapsed',
-            key = 'ss_loc_opt',
-            help = ""
+    if in_opt == 'coord':
+        lat = st.number_input(
+            label = 'Latitude',
+            min_value = -90.0,
+            max_value = 90.0,
+            value = 0.0,
+            step = 0.0001,
+            format = '%.4f',
+            key = 'ss_lat',
+            help = "Use negative value for South"
         )
 
-        if loc_opt == 'name':
-            in_loc = st.text_input(
-                label = 'Location name or postal code',
-                key = 'ss_loc_name',
-                help = "Only 1 character will return empty result, 2 characters will only match exact matching locations, 3 and more characters will perform fuzzy matching."
-            )
-
-            if in_loc != '':
-                loc = find_name(in_loc)
-                lat = loc['latitude']
-                long = loc['longitude']            
-
-        elif loc_opt == 'ip':
-            col_ip_txtin, col_ip_button = st.columns(
-                spec = [0.69, 0.30],
-                gap = 'small'
-            )
-
-            with col_ip_txtin:
-                in_loc = st.text_input(
-                    label = 'IP address',
-                    key = 'ss_loc_ip',
-                    help = "Type location's IP address."
-                )
-            
-            with col_ip_button:
-                st.write('')
-                st.write('')
-                if st.button(
-                    label = 'Get your IP!',
-                    key = 'ss_ip_get',
-                    help = "Click here to get your current device's IP address."
-                ): in_loc = 'me'
-
-            if in_loc != '':
-                loc = find_ip(in_loc)
-                lat = loc['lat']
-                long = loc['long']
-                st.write("The current IP address is:", loc['ip'])
-
-    elif in_opt == 'coord':
-        coord_opt = st.radio(
-            label = 'Choose coordinates notation',
-            options = ('dd', 'dms'),
-            format_func = lambda x: dict_loc_opt.get(x),
-            horizontal = True,
-            key = 'ss_coord_opt',
-            help = ""
+        long = st.number_input(
+            label = 'Longitude',
+            min_value = -180.0,
+            max_value = 180.0,
+            value = 0.0,
+            step = 0.0001,
+            format = '%.4f',
+            key = 'ss_long',
+            help = "Use negative value for West"
         )
 
-        col_lat_sign, col_long_sign = st.columns(
-            spec = 2,
-            gap = 'small'
+    elif in_opt == 'loc':
+        in_loc = st.text_input(
+            label = 'Location name or postal code',
+            key = 'ss_loc_name',
+            help = "Only 1 character will return empty result, 2 characters will only match exact matching locations, 3 and more characters will perform fuzzy matching."
         )
-        
-        with col_lat_sign:
-            lat_sign = st.radio(
-                label = 'Latitude direction',
-                options = ('N', 'S'),
-                format_func = lambda x: dict_loc_opt.get(x),
-                horizontal = True,
-                key = 'ss_lat_sign',
-                help = 'Choose "South" if the latitude contains minus sign.'
-            )
-        
-        with col_long_sign:
-            long_sign = st.radio(
-                label = 'Longitude direction',
-                options = ('E', 'W'),
-                format_func = lambda x: dict_loc_opt.get(x),
-                horizontal = True,
-                key = 'ss_long_sign',
-                help = 'Choose "West" if the longitude contains minus sign.'
-            )
 
-        if coord_opt == 'dd':
-            col_lat, col_long = st.columns(
-                spec = 2,
-                gap = 'small'
-            )
+        if in_loc != '':
+            loc = find_name(in_loc)
+            lat = loc['latitude']
+            long = loc['longitude']
 
-            with col_lat:
-                lat = st.number_input(
-                    label = 'Latitude',
-                    min_value = 0.0,
-                    max_value = 90.0,
-                    value = 0.0,
-                    step = 0.0001,
-                    format = '%.4f',
-                    key = 'ss_lat',
-                    help = ""
-                )
+    elif in_opt == 'ip':
+        in_loc = st.text_input(
+            label = 'IP address',
+            key = 'ss_loc_ip',
+            help = "Type location's IP address."
+        )
 
-            with col_long:
-                long = st.number_input(
-                    label = 'Longitude',
-                    min_value = 0.0,
-                    max_value = 180.0,
-                    value = 0.0,
-                    step = 0.0001,
-                    format = '%.4f',
-                    key = 'ss_long',
-                    help = ""
-                )
+        if st.button(
+            label = 'Get your IP!',
+            key = 'ss_ip_get',
+            help = "Click here to get your current device's IP address."
+        ): in_loc = 'me'
 
-            lat = round(lat, 4)
-            long = round(long, 4)
+        if in_loc != '':
+            loc = find_ip(in_loc)
+            lat = loc['lat']
+            long = loc['long']
+            st.write("The current IP address is:", loc['ip'])
 
-        elif coord_opt == 'dms':
-            with st.expander(
-                label = "Latitude",
-                expanded = True
-            ):
-                lat_deg = st.slider(
-                    label = 'Degrees',
-                    min_value = 0,
-                    max_value = 89,
-                    value = 0,
-                    step = 1,
-                    format = '%s°',
-                    key = 'ss_lat_deg',
-                    help = ""
-                )
-                lat_min = st.slider(
-                    label = 'Minutes',
-                    min_value = 0,
-                    max_value = 59,
-                    value = 0,
-                    step = 1,
-                    format = "%s'",
-                    key = 'ss_lat_min',
-                    help = ""
-                )
-                lat_sec = st.slider(
-                    label = 'Seconds',
-                    min_value = 0,
-                    max_value = 59,
-                    value = 0,
-                    step = 1,
-                    format = '%s"',
-                    key = 'ss_lat_sec',
-                    help = ""
-                )
-            
-            with st.expander(
-                label = "Longitude",
-                expanded = True
-            ):
-                long_deg = st.slider(
-                    label = 'Degrees',
-                    min_value = 0,
-                    max_value = 179,
-                    value = 0,
-                    step = 1,
-                    format = '%s°',
-                    key = 'ss_long_deg',
-                    help = ""
-                )
-                long_min = st.slider(
-                    label = 'Minutes',
-                    min_value = 0,
-                    max_value = 59,
-                    value = 0,
-                    step = 1,
-                    format = "%s'",
-                    key = 'ss_long_min',
-                    help = ""
-                )
-                long_sec = st.slider(
-                    label = 'Seconds',
-                    min_value = 0,
-                    max_value = 59,
-                    value = 0,
-                    step = 1,
-                    format = '%s"',
-                    key = 'ss_long_sec',
-                    help = ""
-                )
-
-            lat = dd(lat_deg, lat_min, lat_sec)
-            long = dd(long_deg, long_min, long_sec)
-
-        if lat_sign == 'north': lat *= 1
-        elif lat_sign == 'south': lat *= -1
-        if long_sign == 'east': long *= 1
-        elif long_sign == 'west': long *= -1
+lat = round(lat, 4)
+long = round(long, 4)
+#if lat == -0.0: lat = 0.0
+#if long == -0.0: long = 0.0
 
 coord = 'latitude=' + str(lat) + '&longitude=' + str(long)
 param += coord
@@ -266,8 +112,6 @@ wx_opt = st.selectbox(
     key = 'ss_wx_opt',
     help = ""
 )
-
-st.write("Result:", wx_opt)
 
 def pref():
     with st.expander(
@@ -379,7 +223,7 @@ if wx_opt == 'fld':
     if ens == True: param += dict_fld['ens']
     elif ens == False: param.replace(dict_fld['ens'], '')
 
-    dt_max = date.today() + relativedelta(months = +7, weeks= +2)
+    dt_max = date.today() + rltvDelta(months = +7, weeks= +2)
     dt_start, dt_end = date.today(), date.today()
     if mdl_consol == True:
         dt_max = date(2022, 7, 31)
@@ -396,5 +240,6 @@ if wx_opt == 'fld':
     st.write("Date:", dt) #debug
     
 #debug:
-st.write('Parameters:', param)
-st.write(loc)
+st.sidebar.write("WX type:", wx_opt)
+st.sidebar.write('Parameters:', param)
+st.sidebar.write('Location:', loc)
